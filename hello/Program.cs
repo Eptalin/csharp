0db0c67d5b1e68e96f4167f2bb7716e0209de8f4
darrell <darrell.kiely.gmail.com>
2026-03-08 12:46:31 +0900

    Add project files.

---
+++ hello/Program.cs
@@ -0,0 +1,14 @@
+﻿// Prompt user for input
+Console.Write("What's your name? ");
+
+// Accept user input
+string name = Console.ReadLine();
+
+// Empty string/null set to World
+if (String.IsNullOrEmpty(name))
+{
+    name = "World";
+}
+
+// Print output
+Console.WriteLine($"Hello, {name}!");
