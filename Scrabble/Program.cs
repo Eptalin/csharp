cc64c2ee93be62bb1b1af9eb1a4fdc8a5d30b661
darrell <darrell.kiely.gmail.com>
2026-03-13 23:10:23 +0900

    Completed CS50x Scrabble problem.

---
+++ Scrabble/Program.cs
@@ -0,0 +1,35 @@
+﻿using Scrabble;
+
+// Prompt players for words
+Word word1 = new(GetWord("Player 1"));
+Word word2 = new(GetWord("Player 2"));
+
+// Report winner
+Console.WriteLine(ReportWinner(word1.Score, word2.Score));
+
+string GetWord(string player)
+{
+    string word;
+    do
+    {
+        Console.Write($"{player}: ");
+        word = Console.ReadLine() ?? "";
+    } while (!word.All(char.IsLetter));
+    return word;
+}
+
+string ReportWinner(int score1, int score2)
+{
+    if (score1 > score2)
+    {
+        return "Player 1 wins!";
+    }
+    else if (score2 > score1)
+    {
+        return "Player 2 wins!";
+    }
+    else
+    {
+        return "Tie!";
+    }
+}
---
+++ Scrabble/Word.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Scrabble
+{
+    internal class Word
+    {
+        // Points for each letter, in order. A:1, B:3, ...
+        private static readonly int[] points =
+        {
+            1, 3, 3, 2, 1, 4, 2, 4, 1, 8, 5, 1, 3,
+            1, 1, 3, 10, 1, 1, 1, 1, 4, 4, 8, 4, 10
+        };
+
+        // Getters, with no setters.
+        public string Text { get; }
+        public int Score { get; }
+        
+        // Constructor
+        public Word(string word)
+        {
+            Text = word.ToUpperInvariant();
+            Score = CalculateScore(Text);
+        }
+
+        // Calculate word score.
+        private static int CalculateScore(string word)
+        {
+            var score = 0;
+            foreach (char letter in word)
+            {
+                score += points[letter - 'A'];
+            }
+            return score;
+        }
+    }
+}
