0ec1f81d5b498c58eb2ef8c8939a154a51cf0a95
darrell <darrell.kiely.gmail.com>
2026-03-15 08:31:11 +0900

    Completed CS50x Readability problem. - Used Passage object which analyses text and stores information about it, which would make it easier to add additional functionality in the future.

---
+++ Readability/Passage.cs
@@ -0,0 +1,62 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Readability
+{
+    internal class Passage
+    {
+        // Private Properties
+        private int letterCount;
+        private int wordCount;
+        private int sentenceCount;
+
+        // Public Properties
+        public string Text { get; }
+
+        public int LetterCount => letterCount;
+        public int WordCount => wordCount;
+        public int SentenceCount => sentenceCount;
+
+        public string ReadingLevel => CalculateReadingLevel();
+
+        // Constructor
+        public Passage(string text)
+        {
+            Text = text;
+            if (text.Length > 0) wordCount = 1;
+            AnalyseText();
+        }
+
+        // Private helpers
+        private void AnalyseText()
+        {
+            foreach (char c in Text)
+            {
+                if (Char.IsLetter(c))
+                {
+                    letterCount++;
+                }
+                if (Char.IsWhiteSpace(c))
+                {
+                    wordCount++;
+                }
+                if (c == '.' || c == '!' || c == '?')
+                {
+                    sentenceCount++;
+                }
+            }
+        }
+
+        private string CalculateReadingLevel()
+        {
+            var L = (double) LetterCount / WordCount * 100;
+            var S = (double) SentenceCount / WordCount * 100;
+            var CLI = (int) Math.Round(0.0588 * L - 0.296 * S - 15.8);
+
+            if (CLI < 1) return "Below Grade 1";
+            else if (CLI > 16) return "Grade 16+";
+            else return $"Grade {CLI}";
+        }
+    }
+}
---
+++ Readability/Program.cs
@@ -0,0 +1,9 @@
+﻿using Readability;
+
+Console.Write("Text: ");
+Passage passage = new(Console.ReadLine() ?? "");
+
+Console.WriteLine($"Letters: {passage.LetterCount}");
+Console.WriteLine($"Words: {passage.WordCount}");
+Console.WriteLine($"Sentences: {passage.SentenceCount}");
+Console.WriteLine($"Reading Level: {passage.ReadingLevel}");
